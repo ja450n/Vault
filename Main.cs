23f087e05d4eac132b168d5ea900164b44f6c9b9
InanZen <peter@guruden.si>
2012-12-09 20:46:58 +0100

    v 0.1 - initiall release

---
+++ Main.cs
@@ -0,0 +1,413 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.IO;
+
+using Terraria;
+using TShockAPI;
+using TShockAPI.DB;
+
+using Hooks;
+using Mono.Data.Sqlite;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+
+namespace Vault
+{
+    internal class BossNPC
+    {
+        public NPC Npc;
+        public Dictionary<int, int> damageData = new Dictionary<int,int>();
+        public BossNPC(NPC npc)
+        {
+            this.Npc = npc;
+        }
+        public void AddDamage(int playerID, int damage)
+        {
+            if (damageData.ContainsKey(playerID))
+                damageData[playerID] += damage;
+            else
+                damageData.Add(playerID, damage);
+        }
+        public Dictionary<int, int> GetRecalculatedReward()
+        {
+            int totalDmg = 0;
+            damageData.Values.ForEach(v => totalDmg += v);
+            float valueMod = (float)totalDmg / (float)Npc.lifeMax;
+            if (valueMod > 1)
+                valueMod = 1;
+            float newValue = (float)Npc.value * valueMod;
+            float valuePerDmg = (float)newValue / (float)totalDmg;
+            //Console.WriteLine("Total dmg: {0}, newValue: {1}, val/dmg: {2}", totalDmg, newValue, valuePerDmg);
+            Dictionary<int, int> returnDict = new Dictionary<int,int>();
+            foreach (KeyValuePair<int,int> kv in damageData)
+                returnDict[kv.Key] = (int)(kv.Value * valuePerDmg);
+            return returnDict;
+        }
+
+    }
+    [APIVersion(1, 12)]
+    public class Vault : TerrariaPlugin
+    {
+        public IDbConnection Database;
+        public String SavePath = Path.Combine(TShock.SavePath, "Vault/");
+        internal PlayerData[] PlayerList = new PlayerData[256];
+        internal Config config;
+        private readonly Random random = new Random();
+        private readonly object _randlocker = new object();
+        public delegate void MoneyEvent(TSPlayer sender, int ammount, int newTotal, HandledEventArgs args);
+        internal List<BossNPC> BossList = new List<BossNPC>();
+        public override string Name
+        {
+            get { return "Vault"; }
+        }
+        public override string Author
+        {
+            get { return "by InanZen"; }
+        }
+        public override string Description
+        {
+            get { return ""; }
+        }
+        public override Version Version
+        {
+            get { return new Version("0.1"); }
+        }
+
+        //-------------------- Static ----------------------------------------------------
+        private static Vault CurrentInstance = null;
+        public static List<MoneyEvent> MoneyEventHandlers = new List<MoneyEvent>();
+        public static int GetBalance(string Name)
+        {
+            try
+            {
+                var Reader = CurrentInstance.Database.QueryReader("SELECT money FROM vault_players WHERE username = @0 AND worldID = @1", Name, Main.worldID);          
+                if (Reader.Read())
+                    return Reader.Get<int>("money");
+            }
+            catch (Exception ex) { Log.ConsoleError(ex.ToString()); }
+            return -1;
+        }
+        public static bool SetBalance(string Name, int Ammount)
+        {
+            try
+            {
+                var player = CurrentInstance.PlayerList.First(p => p.TSPlayer.Name == Name);
+                if (player != null)
+                    player.Money = Ammount;
+                else
+                    CurrentInstance.Database.Query("UPDATE vault_players SET money = @0 WHERE username = @1 AND worldID = @2", Ammount, Name, Main.worldID);
+                return true;
+            }
+            catch (Exception ex) { Log.ConsoleError(ex.ToString()); }
+            return false;
+        }
+        public static bool ModifyBalance(string Name, int Ammount)
+        {
+            try
+            {
+                var player = CurrentInstance.PlayerList.First(p => p.TSPlayer.Name == Name);
+                if (player != null)
+                {
+                    if (player.ChangeMoney(Ammount))
+                        return true;
+                }
+                else
+                {
+                    var Reader = CurrentInstance.Database.QueryReader("SELECT money FROM vault_players WHERE username = @0 AND worldID = @1", Name, Main.worldID);
+                    if (Reader.Read() && Reader.Get<int>("money") >= Ammount * -1)
+                    {
+                        int NewAmmount = Reader.Get<int>("money") + Ammount;
+                        Reader.Dispose();
+                        CurrentInstance.Database.Query("UPDATE vault_players SET money = @0 WHERE username = @1 AND worldID = @2", NewAmmount, Name, Main.worldID);
+                        return true;
+                    }
+                    Reader.Dispose();
+                }
+            }
+            catch (Exception ex) { Log.ConsoleError(ex.ToString()); }
+            return false;
+        }
+        public static Dictionary<int, int> GetPlayerKillCounts(string Name)
+        {
+            Dictionary<int, int> returnDict = new Dictionary<int, int>();
+            try
+            {
+                var reader = CurrentInstance.Database.QueryReader("SELECT killData FROM vault_players WHERE username = @0 AND worldID = @1", Name, Main.worldID);
+                if (reader.Read())
+                    returnDict = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<int, int>>(reader.Get<string>("killData"));
+                reader.Dispose();
+            }
+            catch (Exception ex) { Log.ConsoleError(ex.ToString()); }
+            return returnDict;
+        }
+
+        //--------------------------------------------------------------------------------
+        public Vault(Main game) : base(game)
+        {
+            Order = 1;
+            CurrentInstance = this;
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                NetHooks.GetData -= OnGetData;
+                NetHooks.SendData -= OnSendData;
+                GameHooks.Initialize -= OnInitialize;
+                ServerHooks.Leave -= OnLeave;
+                ServerHooks.Join -= OnJoin;
+
+                Database.Dispose();
+            }
+        }
+        public override void Initialize()
+        {
+            NetHooks.GetData += OnGetData;
+            NetHooks.SendData += OnSendData;
+            GameHooks.Initialize += OnInitialize;            
+            ServerHooks.Leave += OnLeave;
+            ServerHooks.Join += OnJoin;
+        }
+        void OnInitialize()
+        {
+            config = new Config();
+            if (!Directory.Exists(SavePath))
+                Directory.CreateDirectory(SavePath);
+            ReadConfig();
+            switch (TShock.Config.StorageType.ToLower())
+            {
+                case "mysql":
+                    string[] host = TShock.Config.MySqlHost.Split(':');
+                    Database = new MySqlConnection()
+                    {
+                        ConnectionString = string.Format("Server={0}; Port={1}; Database={2}; Uid={3}; Pwd={4};",
+                            host[0],
+                            host.Length == 1 ? "3306" : host[1],
+                            TShock.Config.MySqlDbName,
+                            TShock.Config.MySqlUsername,
+                            TShock.Config.MySqlPassword)
+                    };
+                    break;
+                case "sqlite":
+                    string sql = Path.Combine(SavePath, "vault.sqlite");
+                    Database = new SqliteConnection(string.Format("uri=file://{0},Version=3", sql));
+                    break;
+            }
+            SqlTableCreator sqlcreator = new SqlTableCreator(Database, Database.GetSqlType() == SqlType.Sqlite ? (IQueryBuilder)new SqliteQueryCreator() : new MysqlQueryCreator());
+            sqlcreator.EnsureExists(new SqlTable("vault_players",
+                new SqlColumn("username", MySqlDbType.VarChar) { Length = 30 },
+                new SqlColumn("money", MySqlDbType.Int32),
+                new SqlColumn("worldID", MySqlDbType.Int32),
+                new SqlColumn("killData", MySqlDbType.Text)                
+                ));
+
+        }
+
+        public void OnJoin(int who, HandledEventArgs args)
+        {
+            PlayerList[who] = new PlayerData(this, TShock.Players[who]);
+        }
+        public void OnLeave(int who)
+        {
+            try
+            {
+                PlayerList[who].StopUpdating();
+                PlayerList[who] = null;
+            }
+            catch (Exception ex)
+            {
+                if (who >= 0)
+                    PlayerList[who] = null;
+                Log.ConsoleError(ex.ToString());
+            }
+        }
+        public void OnGetData(GetDataEventArgs e)
+        {
+            /*if (e.Handled)
+                return;*/
+            try
+            {
+                switch (e.MsgID)
+                {
+                    case PacketTypes.PlayerUpdate:
+                        {
+                            byte plyID = e.Msg.readBuffer[e.Index];
+                            byte flags = e.Msg.readBuffer[e.Index + 1];
+                            var player = PlayerList[plyID];
+                            if (player != null && player.LastState != flags)
+                            {
+                                player.LastState = flags;
+                                player.IdleCount = 0;
+                            }
+                            break;
+                        }
+                }
+            }
+            catch (Exception ex) { Log.ConsoleError(ex.ToString()); }
+        }
+        public void OnSendData(SendDataEventArgs e)
+        {
+            try
+            {
+                 switch (e.MsgID)
+                {                               
+                     case PacketTypes.NpcStrike:
+                         {
+                             NPC npc = Main.npc[e.number];
+                            // Console.WriteLine("(SendData) NpcStrike -> 1:{0} 2:{4} 3:{5} 4:{6} 5:{1} remote:{2} ignore:{3}", e.number, e.number5, e.remoteClient, e.ignoreClient, e.number2, e.number3, e.number4);
+                            // Console.WriteLine("NPC: life:{0} reallife:{1}", npc.life);
+                             if (npc != null)
+                             {
+                                 if (npc.boss)
+                                 {
+                                     if (npc.life <= 0)
+                                     {
+                                         for (int i = BossList.Count - 1; i >= 0; i--)
+                                         {
+                                             if (BossList[i].Npc == null)                                             
+                                                 BossList.RemoveAt(i);                                             
+                                             else if (BossList[i].Npc == npc)
+                                             {
+                                                 var rewardDict = BossList[i].GetRecalculatedReward();
+                                                 foreach (KeyValuePair<int, int> reward in rewardDict)
+                                                 {
+                                                     if (PlayerList[reward.Key] != null)
+                                                         PlayerList[reward.Key].ChangeMoney(reward.Value, config.AnnounceBossGain);
+                                                 } 
+                                                 BossList.RemoveAt(i);  
+                                             }
+                                             else if (!BossList[i].Npc.active)
+                                                 BossList.RemoveAt(i);                                             
+                                         }
+
+                                         if (e.ignoreClient >= 0)
+                                         {
+                                              var player = PlayerList[e.ignoreClient];
+                                              if (player != null)
+                                                  player.AddKill(npc.netID);
+                                         }
+                                     }
+                                     else if (e.ignoreClient >= 0)
+                                     {
+                                         var bossnpc = BossList.Find(n => n.Npc == npc);
+                                         if (bossnpc != null)
+                                             bossnpc.AddDamage(e.ignoreClient, (int)e.number2);
+                                         else
+                                         {
+                                             BossNPC newBoss = new BossNPC(npc);
+                                             newBoss.AddDamage(e.ignoreClient, (int)e.number2);
+                                             BossList.Add(newBoss);
+                                         }
+                                     }
+                                 }
+                                 else if (npc.life <= 0 && e.ignoreClient >= 0)
+                                 {
+                                     var player = PlayerList[e.ignoreClient];
+                                     if (player != null)
+                                     {
+                                         if (npc.value > 0)
+                                         {
+                                             float Mod = 1;
+                                             if (player.TSPlayer.TPlayer.buffType.Contains(13)) // battle potion
+                                                 Mod *= config.BattlePotionModifier;
+                                             if (config.OptionalMobModifier.ContainsKey(npc.netID))
+                                                 Mod *= config.OptionalMobModifier[npc.netID]; // apply custom modifiers                                        
+
+                                             int minVal = (int)((npc.value - (npc.value * 0.1)) * Mod);
+                                             int maxVal = (int)((npc.value + (npc.value * 0.1)) * Mod);
+                                             int rewardAmt = RandomNumber(minVal, maxVal);
+                                             player.ChangeMoney(rewardAmt, config.AnnounceKillGain);
+                                         }
+                                         player.AddKill(npc.netID);
+                                     }
+                                 }
+                             }
+
+                             break;
+                         }
+                }
+            }            
+            catch (Exception ex) { Log.ConsoleError(ex.ToString()); }
+        }
+
+
+        internal class Config
+        {
+            public int InitialMoney = 0;
+            public float BattlePotionModifier = 0.75f;
+            public Dictionary<int, float> OptionalMobModifier = new Dictionary<int, float>();
+            public bool GiveTimedPay = true;
+            public byte PayEveryMinutes = 10;
+            public byte MaxIdleTime = 3;
+            public int PayAmmount = 5000;
+            public bool AnnounceTimedPay = false;
+            public bool AnnounceKillGain = false;
+            public bool AnnounceBossGain = true;
+            
+        }
+        private void CreateConfig()
+        {
+            string filepath = Path.Combine(SavePath, "config.json");
+            try
+            {
+                using (var stream = new FileStream(filepath, FileMode.Create, FileAccess.Write, FileShare.Write))
+                {
+                    using (var sr = new StreamWriter(stream))
+                    {
+                        config = new Config();
+                        var configString = JsonConvert.SerializeObject(config, Formatting.Indented);
+                        sr.Write(configString);
+                    }
+                    stream.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.ConsoleError(ex.Message);
+                config = new Config();
+            }
+        }
+        private bool ReadConfig()
+        {
+            string filepath = Path.Combine(SavePath, "config.json");
+            try
+            {
+                if (File.Exists(filepath))
+                {
+                    using (var stream = new FileStream(filepath, FileMode.Open, FileAccess.Read, FileShare.Read))
+                    {
+                        using (var sr = new StreamReader(stream))
+                        {
+                            var configString = sr.ReadToEnd();
+                            config = JsonConvert.DeserializeObject<Config>(configString);
+                        }
+                        stream.Close();
+                    }
+                    return true;
+                }
+                else
+                {
+                    Log.ConsoleError("Vault config not found. Creating new one");
+                    CreateConfig();
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.ConsoleError(ex.Message);
+            }
+            return false;
+        }
+        internal int RandomNumber(int min, int max)
+        {
+            lock (_randlocker)
+            {
+                return random.Next(min, max);
+            }
+        }
+    }
+}
---
+++ PlayerData.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using TShockAPI;
+using TShockAPI.DB;
+using Terraria;
+using System.Threading;
+namespace Vault
+{
+    internal class PlayerData
+    {
+        private Vault main;
+        public TSPlayer TSPlayer;
+        public byte LastState = 0;
+        public byte IdleCount = 0;
+        private int money;
+        public int Money
+        {
+            get { return money; }
+            set
+            {
+                main.Database.Query("UPDATE vault_players SET money = @0 WHERE username = @1 AND worldID = @2", value, TSPlayer.Name, Main.worldID);
+                money = value;
+            }
+        }
+        public bool ChangeMoney(int ammount, bool announce = false)
+        {
+            HandledEventArgs args = new HandledEventArgs();
+            if (this.money >= ammount * -1)
+            {
+                foreach (var action in Vault.MoneyEventHandlers)
+                {
+                    if (action != null)
+                        action(this.TSPlayer, ammount, this.money + ammount, args);
+                }
+                if (!args.Handled)
+                {
+                    this.Money += ammount;
+                    if (announce)
+                        TSPlayer.SendMessage(String.Format("You've {1} {0}", MoneyToString(ammount), ammount >= 0 ? "gained" : "lost"), Color.DarkOrange);
+                    return true;
+                }
+            }
+            return false;
+        }
+        public Dictionary<int, int> GetKillCounts()
+        {
+            Dictionary<int, int> returnDict = new Dictionary<int, int>();
+            var reader = main.Database.QueryReader("SELECT killData FROM vault_players WHERE username = @0 AND worldID = @1", TSPlayer.Name, Main.worldID);
+            if (reader.Read())
+                returnDict = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<int, int>>(reader.Get<string>("killData"));
+            reader.Dispose();
+            return returnDict;
+        }
+        public void AddKill(int mobID)
+        {
+            var killDict = GetKillCounts();
+            if (killDict.ContainsKey(mobID))
+                killDict[mobID] += 1;
+            else
+                killDict.Add(mobID, 1);
+            main.Database.Query("UPDATE vault_players SET killData = @0 WHERE username = @1 AND worldID = @2", Newtonsoft.Json.JsonConvert.SerializeObject(killDict), TSPlayer.Name, Main.worldID);
+        }
+        public PlayerData(Vault instance, TSPlayer player)
+        {
+            main = instance;
+            TSPlayer = player;
+            UpdatePlayerData();
+            if (main.config.GiveTimedPay)
+                StartUpdating();
+        }
+        public void UpdatePlayerData()
+        {
+            QueryResult result = main.Database.QueryReader("SELECT * FROM vault_players WHERE username = @0 AND worldID = @1", TSPlayer.Name, Main.worldID);
+            if (result.Read())
+            {
+                this.money = result.Get<int>("money");
+            }
+            else
+            {
+                this.money = main.config.InitialMoney;
+                main.Database.Query("INSERT INTO vault_players(username, money, worldID, killData) VALUES(@0,@1,@2,@3)", TSPlayer.Name, this.money, Main.worldID, Newtonsoft.Json.JsonConvert.SerializeObject(new Dictionary<int,int>()));
+            }
+            result.Dispose();
+        }
+
+        public static string MoneyToString(int ammount)
+        {
+            int[] money = MoneyToArray(ammount);
+            StringBuilder builder = new StringBuilder(50);
+            if (money[3] > 0)
+                builder.AppendFormat("{0} platinum {1} gold {2} silver {3} copper", money[3], money[2], money[1], money[0]);
+            else if (money[2] > 0)
+                builder.AppendFormat("{0} gold {1} silver {2} copper", money[2], money[1], money[0]);
+            else if (money[1] > 0)
+                builder.AppendFormat("{0} silver {1} copper", money[1], money[0]);
+            else
+                builder.AppendFormat("{0} copper", money[0]);
+            return builder.ToString();
+        }
+        public static int[] MoneyToArray(int ammount)
+        {
+            int[] moneyArray = new int[4];
+            moneyArray[0] = ammount % 100;
+            moneyArray[1] = ammount % 10000;
+            moneyArray[2] = ammount % 1000000;
+            moneyArray[3] = (int)Math.Floor(ammount / 1000000d);
+            moneyArray[2] = (int)((moneyArray[2] - moneyArray[1]) / 10000);
+            moneyArray[1] = (int)((moneyArray[1] - moneyArray[0]) / 100);
+            return moneyArray;
+        }
+
+
+        public Thread UpdateThread = null;
+        public void StartUpdating()
+        {
+            try
+            {
+                if (this.UpdateThread == null || !this.UpdateThread.IsAlive)
+                {
+                    var updater = new Updater(main, TSPlayer.Index);
+                    this.UpdateThread = new Thread(updater.PayTimer);
+                    this.UpdateThread.Start();
+                }
+            }
+            catch (Exception ex) { Log.ConsoleError(ex.ToString()); }
+        }
+        public void StopUpdating()
+        {
+            try
+            {
+                if (this.UpdateThread != null)
+                    this.UpdateThread.Abort();
+            }
+            catch (Exception ex) { Log.ConsoleError(ex.ToString()); }
+        }
+
+        // -------------------------------- UPDATER ----------------------------------------------------------
+        private class Updater
+        {
+            int who;
+            Vault main;
+            byte TimerCount;
+            public Updater(Vault instance, int who)
+            {
+                this.main = instance;
+                this.who = who;
+                this.TimerCount = 0;
+            }
+            public void PayTimer()
+            {
+                while (Thread.CurrentThread.IsAlive && main != null)
+                {
+                    var player = main.PlayerList[who];
+                    if (player != null)
+                    {
+                        try
+                        {
+                            if (player.IdleCount < main.config.MaxIdleTime)
+                            {
+                                player.IdleCount++;
+                                if (this.TimerCount == main.config.PayEveryMinutes)
+                                    player.ChangeMoney(main.config.PayAmmount, main.config.AnnounceTimedPay);
+                                this.TimerCount++;
+                                if (this.TimerCount > main.config.PayEveryMinutes)
+                                    this.TimerCount = 1;
+                            }
+                        }
+                        catch (Exception ex) { Log.ConsoleError(ex.ToString()); }
+                        Thread.Sleep(60000);
+                    }
+                    else
+                        Thread.CurrentThread.Abort();
+                }
+            }
+        }
+
+    }
+}
